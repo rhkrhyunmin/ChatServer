e8490d7575fe1c0bd46377502d6e44b9e9f23cc2
hyunmin <98993393+rhkrhyunmin@users.noreply.github.com>
2024-03-29 20:51:27 +0900

    Add files via upload

---
+++ chat.js
@@ -0,0 +1,58 @@
+"use strict"
+const socket = io();
+
+const nickname = document.querySelector("#nickname");
+const chatList = document.querySelector(".chatting-list");
+const chatinput = document.querySelector(".chatting-input");
+const sendButton = document.querySelector(".send-button");
+const disPlayContainer = document.querySelector(".display-container");
+
+chatinput.addEventListener("keypress", (event)=>
+{
+    if(event.keycode === 13)
+    {
+        send();
+        chatinput.value = "";
+    }
+})
+
+function send()
+{
+    const param = {
+        name: nickname.value,
+        msg: chatinput.value
+    }
+
+    socket.emit("chatting",param)
+}
+
+sendButton.addEventListener("click",send)
+
+socket.on("chatting", (data)=> {
+   const {name,msg,time} = data;
+   const item = new Limodel(name, msg, time);
+   item.makeLi(); 
+   disPlayContainer.scrollTop = 0;
+})
+
+function Limodel(name, msg, time)
+{
+    this.name = name;
+    this.msg = msg;
+    this.time = time;
+
+    this.makeLi = ()=>
+    {
+        const li = document.createElement("li");
+        li.classList.add(nickname.value == this.name ? "sent" : "received")
+        const dom = `<span class="proflie">
+        <span class="user">${this.name}</span>
+        <img class="image" src="https://placeimg.com/200/50/any" alt="any">
+    </span>
+    <span class = "message">${this.msg}</span>
+    <span class ="time">${this.time}</span>`;
+    li.innerHTML = dom;
+    chatList.appendChild(li);
+    }
+    
+}
